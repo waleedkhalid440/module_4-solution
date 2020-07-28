e2d06dd81708c9de3f76aaa312607d9c5b815597
waleedkhalid440 <65292620+waleedkhalid440@users.noreply.github.com>
2020-07-28 23:20:03 +0500

    done

--- js/SpeakGoodBye.js
+++ js/SpeakGoodBye.js
@@ -3,19 +3,22 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-(function () {
+(function (window) {
   var byeSpeaker = {};
-  byeSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  //byeSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
   var speakWord = "Good Bye";
 
   // STEP 8: Rewrite the 'speak' function such that it is attached to the
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  byeSpeaker.speak = function () {
-    console.log(speakWord + " " + byeSpeaker.names);
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
-})();
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -1,19 +1,22 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-(function () {
+(function (window) {
   var helloSpeaker = {};
-  helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  //helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
   var speakWord = "Hello";
 
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
   // helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  helloSpeaker.speak = function () {
-    console.log(speakWord + " " + helloSpeaker.names);
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
-})();
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1

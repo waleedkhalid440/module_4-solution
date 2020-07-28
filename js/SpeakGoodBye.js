5d54ca2df7b5ef49090189b6e6024be8917323dc
waleedkhalid440 <65292620+waleedkhalid440@users.noreply.github.com>
2020-07-28 22:44:42 +0500

    done

--- js/SpeakGoodBye.js
+++ js/SpeakGoodBye.js
@@ -5,15 +5,15 @@
 // See Lecture 52, part 2
 (function () {
   var byeSpeaker = {};
-
+  byeSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
   var speakWord = "Good Bye";
 
   // STEP 8: Rewrite the 'speak' function such that it is attached to the
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+  byeSpeaker.speak = function () {
+    console.log(speakWord + " " + byeSpeaker.names);
   }
 })();
 
--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -3,15 +3,15 @@
 
 (function () {
   var helloSpeaker = {};
-
+  helloSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
   var speakWord = "Hello";
 
   // STEP 4: Rewrite the 'speak' function such that it is attached to the
   // helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+  helloSpeaker.speak = function () {
+    console.log(speakWord + " " + helloSpeaker.names);
   }
 })();
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
